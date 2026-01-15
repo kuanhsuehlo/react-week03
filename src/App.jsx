fe22ecf5f78e517397e651015abf35cd3ad3ec07
kuanhsuehlo <woody40408@gmail.com>
2026-01-15 16:19:39 +0800

    update

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './assets/all.scss'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>你好</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
